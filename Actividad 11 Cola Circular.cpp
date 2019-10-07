06f8c5578a5f30258d57358f2d5aa72d36da30db
OscarCornejo00 <54414881+OscarCornejo00@users.noreply.github.com>
2019-10-06 18:14:11 -0600

    Actividad 11

---
+++ Actividad 11 Cola Circular.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int valor=1,elementos=0,inicio=0,posicion=0,opcion=1;
+    int colacir[5]; 
+    while(opcion!=0){
+        switch(opcion){
+            case 1 : 
+                cout<<"ingresa un valor"<<endl;
+                cin>>valor;
+                posicion=(inicio+elementos)%5;
+                colacir[posicion]=valor;
+                cout<<"Valor ingresado : "<<colacir[posicion]<<" en la posicion " <<posicion<<endl;
+                elementos++; 
+                break;
+            case 2:
+                if(elementos!=0){
+                    cout<<"Eliminando ..."<<endl;
+                    cout<<"Fue eliminado el elemento "<<colacir[inicio]<<" de la posicion " <<inicio<<endl;
+                    colacir[inicio]=NULL; 
+                    inicio=(inicio+1)%5;
+                    elementos--;
+                }
+                break;
+        }
+         cout<<"ingresa 1 para agregar un elemento , 2 para eliminar un elemento o 0 para salir "<<endl;
+         cin>>opcion;
+    }
+    return 0;
+}
